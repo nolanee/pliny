0c513c2490b9a79c164add74d0a239aaa4ff054b
Edward Nolan <nolanee@umich.edu>
2021-04-01 16:59:41 -0400

    updated tests

--- test.py
+++ test.py
@@ -12,15 +12,4 @@ assert("C. Plinius Baebio Hispano suo s." in pliny_letters(Pliny_books.I)[24])
 assert(list_greek(Pliny_books.I) == ['I.2', 'I.5', 'I.7', 'I.9', 'I.12', 'I.18', 'I.20'])
 assert(list_greek(Pliny_books.X) == ['X.10', 'X.15', 'X.116', 'X.117', 'X.118', 'X.119'])
 
-lists_storage = []
-for book in Pliny_books:
-    lists_storage.append(list_greek(book))
 
-passage_list = ""
-for li in lists_storage:
-    passage_list += ', '.join(li)
-    passage_list += '\n'
-
-file = open("results.txt", 'w')
-file.write(passage_list)
-file.close
